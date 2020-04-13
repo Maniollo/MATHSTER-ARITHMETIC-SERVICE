7d07fe8771083472339a71f45277abbfbfaf21aa
maniollo <mariusz.maslanka@gmail.com>
2020-04-13 19:12:27 +0200

    T0001: Add Travis CI file.

---
+++ src/test/groovy/marmas/arithmetic/DummyUnitSpec.groovy
@@ -0,0 +1,11 @@
+package marmas.arithmetic
+
+import spock.lang.Specification
+
+
+class DummyUnitSpec extends Specification {
+    def "should be valid"() {
+        expect:
+        4 == 4
+    }
+}
