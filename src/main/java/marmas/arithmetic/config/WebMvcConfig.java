091c4345627ee580613896b28dbd0d2d64917c54
maniollo <mariusz.maslanka@gmail.com>
2020-05-24 15:28:40 +0200

    T0005: Add CORS for UI url.

--- src/main/java/marmas/arithmetic/config/WebMvcConfig.java
+++ src/main/java/marmas/arithmetic/config/WebMvcConfig.java
@@ -1,13 +1,29 @@
 package marmas.arithmetic.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-class WebMvcConfig {
+class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${mathster.ui.url}")
+    private String mathsterUrl;
+
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
+    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins(mathsterUrl)
+                .allowedHeaders("*");
     }
 }
